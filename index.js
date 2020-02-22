f4def9fa35189ed6e38542f315d375e441e8849f
Saiko <a.m.abdelmottaleb@gmail.com>
2020-02-23 01:06:18 +0200

    test graphql code structure

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const {graphql, buildSchema} = require('graphql')
 
+const db = {
+    users: [
+        {id: '1', email:'test1@test.com', name:'test1'},
+        {id: '2', email:'test2@test.com', name:'test2'},
+        {id: '3', email:'test3@test.com', name:'test3'}
+    ]
+}
+
 const schema = buildSchema(`
     type Query{
-        message: String
+        users: [User!]!
+    }
+
+    type User{
+        id: ID!
+        email: String!
+        name: String
+        avatarUrl: String
     }
 `)
 
 const rootValue = {
-    message: () => ' GrqphQL Works'
+    users: () => db.users
 }
 
 graphql(
     schema,
     `{
-        message
+        users {
+            id
+            email
+        }
     }`,
     rootValue
 ).then(
-    console.log
+    res => console.dir(res, { depth: null})
 ).catch(
     console.error
 )
