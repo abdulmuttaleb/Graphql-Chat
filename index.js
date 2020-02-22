c805a707737b4a678eedb6c1c5dae4498c6ce40f
Saiko <a.m.abdelmottaleb@gmail.com>
2020-02-23 01:23:02 +0200

    adding express on top of project

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const {graphql, buildSchema} = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const {buildSchema} = require('graphql')
+const crypto = require('crypto')
 
 const db = {
     users: [
-        {id: '1', email:'test1@test.com', name:'test1'},
-        {id: '2', email:'test2@test.com', name:'test2'},
-        {id: '3', email:'test3@test.com', name:'test3'}
+        {id: '11', email:'test1@test.com', name:'test1'},
+        {id: '21', email:'test2@test.com', name:'test2'},
+        {id: '31', email:'test3@test.com', name:'test3'}
     ]
 }
 
 const schema = buildSchema(`
     type Query{
         users: [User!]!
+    }
+
+    type Mutation{
+        addUser(email:String!, name:String): User
     }
 
     type User{
@@ -22,20 +29,27 @@ const schema = buildSchema(`
 `)
 
 const rootValue = {
-    users: () => db.users
+    users: () => db.users,
+
+    addUser: ({email, name}) =>{
+        const user = {
+            id: crypto.randomBytes(10).toString('hex'),
+            email,
+            name
+        }
+
+        db.users.push(user)
+
+        return user
+    }
 }
 
-graphql(
-    schema,
-    `{
-        users {
-            id
-            email
-        }
-    }`,
-    rootValue
-).then(
-    res => console.dir(res, { depth: null})
-).catch(
-    console.error
-)
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+   schema,
+   rootValue,
+   graphiql : true
+}))
+
+app.listen(3000, () => console.log('Listening on 3000'))
