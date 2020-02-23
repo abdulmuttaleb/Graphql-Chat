ae72230d8e2868024d3b13123d5f78a5356756ce
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-02-23 14:24:02 +0200

    fetching messages along user data

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const db = {
     ]
 }
 
+class User{
+    constructor(user){
+        Object.assign(this, user)
+    }
+
+    messages(){
+        return db.messages.filter(message => message.userId === this.id)
+    }
+}
+
 const schema = buildSchema(`
     type Query{
         users: [User!]!
@@ -34,6 +44,7 @@ const schema = buildSchema(`
         email: String!
         name: String
         avatarUrl: String
+        messages: [Message!]!
     }
 
     type Message{
@@ -45,7 +56,7 @@ const schema = buildSchema(`
 `)
 
 const rootValue = {
-    users: () => db.users,
+    users: () => db.users.map( user => new User(user)),
 
     user: args => db.users.find( user => user.id === args.id),
 
